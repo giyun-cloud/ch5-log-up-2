9b3d2d30b58bb63eacd922adc900700f1bbc1421
giyun-cloud <nangiyoon@gmail.com>
2021-09-25 07:45:16 +0900

    1-app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
-export default class App {
+import temaplte from './app.template';
+import { AnyObject } from './types';
 
+
+
+export default class App {
+  template = temaplte;
+  data: AnyObject;
+  container: HTMLElement;
+  fields: AnyObject[];
+  active: boolean = false;
+
+  constructor(container: string, data: AnyObject = {}) {
+    this.container = document.querySelector(container) as HTMLElement;
+    this.data = data;
+    this.fields = [];
+
+    this.initialize();
+
+    setInterval()
+  }
 }
